88e8165c1de5b846b2fd2bac8f9b36111736c446
taiki1frsh <taiki.furusho@gmail.com>
2023-01-31 10:26:00 +0900

    comment out any option for optimizer

--- yf-test-on-osmosis/ci-scripts/compile_and_optimize.sh
+++ yf-test-on-osmosis/ci-scripts/compile_and_optimize.sh
@@ -19,7 +19,7 @@ cd $SCRIPT_DIR/../
 #   cosmwasm/rust-optimizer:0.12.11
   
 # incase for m1
-docker run --rm -v "$(pwd)":/code \
-  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
-  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/rust-optimizer-arm64:0.12.11
+# docker run --rm -v "$(pwd)":/code \
+#   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+#   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+#   cosmwasm/rust-optimizer-arm64:0.12.11
