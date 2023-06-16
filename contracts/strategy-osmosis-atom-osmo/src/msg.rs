102dab89e299863398c34d9dc948edc17eb58061
jununifi <jun.ununifi@outlook.com>
2023-06-16 22:52:28 +0800

    bootstrap osmosis-atom-osmo strategy from comdex contract + add variables required for ibc strategy management

---
+++ contracts/strategy-osmosis-atom-osmo/src/msg.rs
@@ -0,0 +1,147 @@
+use crate::state::ChannelInfo;
+use cosmwasm_std::{Coin, Decimal, Uint128};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub unbond_period: u64,
+    pub deposit_denom: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    UpdateConfig {
+        owner: Option<String>,
+        unbond_period: Option<u64>,
+        deposit_denom: Option<String>,
+    },
+    Stake(StakeMsg),
+    Unstake(UnstakeMsg),
+    AddRewards(AddRewardsMsg),
+    IbcTransferToHost(IbcTransferToHostMsg),
+    IbcTransferToController(IbcTransferToControllerMsg),
+    IcaAddLiquidity(IcaAddLiquidityMsg),
+    IcaRemoveLiquidity(IcaRemoveLiquidityMsg),
+    IcaSwapRewardsToTwoTokens(IcaSwapRewardsToTwoTokensMsg),
+    IcaSwapTwoTokensToDepositToken(IcaSwapTwoTokensToDepositTokenMsg),
+    IcaSwapDepositTokenToTwoTokens(IcaSwapDepositTokenToTwoTokensMsg),
+    StoreIcaUnlockedBalances(StoreIcaUnlockedBalancesMsg),
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct IbcTransferToHostMsg {
+    pub ica_channel_id: String,
+    pub channel_id: String,
+    pub denom: String,
+    pub amount: Uint128,
+    pub timeout: u64,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct IbcTransferToControllerMsg {
+    pub channel_id: String,
+    pub denom: String,
+    pub amount: Uint128,
+    pub timeout: u64,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct IcaAddLiquidityMsg {
+    pub channel_id: String,
+    pub denom: String,
+    pub amount: Uint128,
+    pub timeout: u64,
+    pub val_addr: String, // TODO: temporary for MsgDelegate test - should remove
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct IcaRemoveLiquidityMsg {
+    pub channel_id: String,
+    pub denom: String,
+    pub amount: Uint128,
+    pub timeout: u64,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct IcaSwapRewardsToTwoTokensMsg {
+    pub channel_id: String,
+    pub denom: String,
+    pub amount: Uint128,
+    pub timeout: u64,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct IcaSwapTwoTokensToDepositTokenMsg {
+    pub channel_id: String,
+    pub denom: String,
+    pub amount: Uint128,
+    pub timeout: u64,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct IcaSwapDepositTokenToTwoTokensMsg {
+    pub channel_id: String,
+    pub denom: String,
+    pub amount: Uint128,
+    pub timeout: u64,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct StoreIcaUnlockedBalancesMsg {
+    pub coins: Vec<Coin>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct StakeMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct UnstakeMsg {
+    pub amount: Uint128,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct AddRewardsMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    Config {},
+    Bonded {
+        addr: String,
+    },
+    Unbonding {
+        addr: String,
+    },
+    Fee {},
+    /// Show all channels connected to.
+    ListChannels {},
+    /// Returns the details of the name channel, error if not created.
+    Channel {
+        id: String,
+    },
+    // TODO: add more queries to get exact state of the contract
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct FeeInfo {
+    pub deposit_fee_rate: Decimal,
+    pub withdraw_fee_rate: Decimal,
+    pub interest_fee_rate: Decimal,
+}
+
+/// We currently take no arguments for migrations
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct MigrateMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ListChannelsResponse {
+    pub channels: Vec<ChannelInfo>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ChannelResponse {
+    /// Information on the channel's connection
+    pub info: ChannelInfo,
+}
---
+++ contracts/strategy-osmosis-atom-osmo/src/state.rs
@@ -0,0 +1,100 @@
+use cosmwasm_std::{
+    Addr, Binary, BlockInfo, IbcEndpoint, Order, StdResult, Storage, Timestamp, Uint128,
+};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cw_storage_plus::{Item, Map};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ControllerConfig {
+    pub deposit_denom: String, // `ibc/xxxxuatom`
+    pub free_amount: Uint128,
+    pub pending_transfer_amount: Uint128, // TODO: where to get hook for transfer finalization?
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct HostConfig {
+    pub lp_denom: String, // ATOM-OSMO
+    pub bonded_lp_amount: Uint128,
+    pub unbonding_lp_amount: Uint128,
+    pub free_lp_amount: Uint128,
+    pub pending_bond_lp_amount: Uint128,
+    pub pending_unbond_lp_amount: Uint128,
+    pub pending_swap_lp_amount: Uint128, // pending swap from lp to deposit token amount
+
+    pub osmo_denom: String, // OSMO
+    pub free_osmo_amount: Uint128,
+    pub pending_swap_to_atom_amount: Uint128, // Convert OSMO to ATOM
+
+    pub atom_denom: String,                    // ATOM
+    pub free_atom_amount: Uint128,             // free ATOM balance
+    pub pending_swap_to_osmo_amount: Uint128,  // pending swap from ATOM -> OSMO to add liquidity
+    pub pending_add_liquidity_amount: Uint128, // amount of ATOM used on liquidity addition
+    pub pending_transfer_amount: Uint128, // pending transfer to controller - TODO: how to get hook for transfer finalization?
+    pub required_withdrawal_amount: Uint128,
+    // TODO: probably create two ica accounts for convenience
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Config {
+    pub owner: Addr,
+    pub unbond_period: u64,
+    pub lp_redemption_rate: Uint128,
+    pub total_deposit: Uint128,
+    pub total_withdrawal: Uint128,
+
+    pub channel_id: String,
+    pub host_config: HostConfig,
+    pub controller_config: ControllerConfig,
+}
+
+pub const CONFIG: Item<Config> = Item::new("config");
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct DepositInfo {
+    pub sender: Addr,
+    pub amount: Uint128,
+}
+
+pub const DEPOSITS: Map<String, DepositInfo> = Map::new("deposits");
+
+pub const CHANNEL_INFO: Map<&str, ChannelInfo> = Map::new("channel_info");
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct ChannelInfo {
+    /// id of this channel
+    pub id: String,
+    /// the remote channel/port we connect to
+    pub counterparty_endpoint: IbcEndpoint,
+    /// the connection this exists on (you can use to query client/consensus info)
+    pub connection_id: String,
+    /// interchain account address
+    pub address: String,
+}
+
+/// Metadata defines a set of protocol specific data encoded into the ICS27 channel version bytestring
+/// See ICS004: <https://github.com/cosmos/ibc/tree/master/spec/core/ics-004-channel-and-packet-semantics#Versioning>
+#[derive(Clone, PartialEq, Serialize, Deserialize)]
+pub struct Metadata {
+    /// version defines the ICS27 protocol version
+    pub version: String,
+    /// controller_connection_id is the connection identifier associated with the controller chain
+    pub controller_connection_id: String,
+    /// host_connection_id is the connection identifier associated with the host chain
+    pub host_connection_id: String,
+    /// address defines the interchain account address to be fulfilled upon the OnChanOpenTry handshake step
+    /// NOTE: the address field is empty on the OnChanOpenInit handshake step
+    pub address: String,
+    /// encoding defines the supported codec format
+    pub encoding: String,
+    /// tx_type defines the type of transactions the interchain account can execute
+    pub tx_type: String,
+}
+
+#[derive(Clone, PartialEq, Serialize, Deserialize)]
+pub struct InterchainAccountPacketData {
+    pub r#type: i32,
+    pub data: Vec<u8>,
+    pub memo: String,
+}
