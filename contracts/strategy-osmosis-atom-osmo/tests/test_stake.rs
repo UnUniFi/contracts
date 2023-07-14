30bd64ec85f361edf2d39c26ac2bd82d37cc5c3f
taiki1frsh <taiki.furusho@gmail.com>
2023-07-14 13:14:21 +0900

    simple unit test

---
+++ contracts/strategy-osmosis-atom-osmo/tests/test_stake.rs
@@ -0,0 +1,32 @@
+use cosmwasm_std::{coins, Uint128};
+use cosmwasm_std::testing::{mock_env, mock_info};
+use helpers::th_query;
+use strategy::error::ContractError;
+use strategy::strategy::{ExecuteMsg, StakeMsg};
+use strategy_osmosis_atom_osmo::contract::{execute_update_config, execute_stake};
+use strategy_osmosis_atom_osmo::msg::{UpdateConfigMsg, QueryMsg};
+use strategy_osmosis_atom_osmo::state::{Config};
+
+use crate::helpers::setup;
+
+mod helpers;
+
+#[test]
+fn stake() {
+    let mut deps = setup();
+    let sender = "anyone";
+
+    let invalid_info = mock_info("anyone", &coins(10000 as u128, "invalid"));
+    let err = execute_stake(deps.as_mut(), mock_env(), invalid_info.funds[0].clone(), invalid_info.sender).unwrap_err();
+    assert_eq!(err, ContractError::NoAllowedToken {  });
+
+    let config: Config = th_query(deps.as_ref(), QueryMsg::Config {});
+
+    let info = mock_info(sender, &coins(10000 as u128, "stake"));
+    let res = execute_stake(deps.as_mut(), mock_env(), info.funds[0].clone(), info.sender).unwrap();
+
+    assert_eq!(0, res.messages.len());
+
+    let deposit: Config = th_query(deps.as_ref(), QueryMsg::Config {  });
+    assert_eq!(Uint128::from(10000 as u64), deposit.total_deposit);
+}
