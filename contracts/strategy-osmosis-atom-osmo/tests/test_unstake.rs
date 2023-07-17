1fc6c9a7a6a507fc40506f67878d4799c822414a
taiki1frsh <taiki.furusho@gmail.com>
2023-07-17 12:38:23 +0900

    test for unstake with correct response

--- contracts/strategy-osmosis-atom-osmo/tests/test_unstake.rs
+++ contracts/strategy-osmosis-atom-osmo/tests/test_unstake.rs
@@ -1,7 +1,8 @@
-use cosmwasm_std::{coins, Uint128};
+use cosmwasm_std::{coins, Uint128, StdError, OverflowError};
+// use cosmwasm_std::Overflow;
 use cosmwasm_std::testing::{mock_env, mock_info};
 use helpers::th_query;
-use strategy::error::ContractError;
+use strategy::error::{ContractError, NoDeposit};
 use strategy::strategy::{ExecuteMsg, StakeMsg};
 use strategy_osmosis_atom_osmo::contract::{execute_update_config, execute_stake, execute_unstake};
 use strategy_osmosis_atom_osmo::msg::{UpdateConfigMsg, QueryMsg};
@@ -18,25 +19,18 @@ fn unstake() {
 
     // Error: because of no deposit token
     let info = mock_info(sender, &coins(10000 as u128, "stake"));
-    let err = execute_unstake(deps.as_mut(), info.funds[0].amount.clone(), info.sender).unwrap();
-    println!("{:?}", err);
-    // assert_eq!();
+    let err = execute_unstake(deps.as_mut(), info.funds[0].amount.clone(), info.sender).unwrap_err();
+    assert_eq!(err, ContractError::Std(NoDeposit{}.into()));
 
     // Error: because of insufficient deposit
     let stake_info = mock_info(sender, &coins(100 as u128, "stake"));
     execute_stake(deps.as_mut(), mock_env(), stake_info.funds[0].clone(), stake_info.sender);
     let unstake_info = mock_info(sender, &coins(10000 as u128, "stake"));
-    let err = execute_unstake(deps.as_mut(), unstake_info.funds[0].amount.clone(), unstake_info.sender).unwrap();
-    // Below should be true in the future
-    // assert_eq!(err, ContractError::AmountOverflow {  });
+    let result = execute_unstake(deps.as_mut(), unstake_info.funds[0].amount.clone(), unstake_info.sender);
+    assert!(result.is_err(), "overflow");
 
     // Success:
-
-    // stake first
-    let stake_info = mock_info(sender, &coins(10000 as u128, "stake"));
-    execute_stake(deps.as_mut(), mock_env(), stake_info.funds[0].clone(), stake_info.sender);
-    
-    let unstake_info = mock_info(sender, &coins(100 as u128, "stake"));
+    let unstake_info = mock_info(sender, &coins(10 as u128, "stake"));
     let res = execute_unstake(deps.as_mut(), unstake_info.funds[0].amount.clone(), unstake_info.sender).unwrap();
-    assert_eq!(100, res.attributes[1].value.parse::<u128>().unwrap());
+    assert_eq!(10, res.attributes[1].value.parse::<u128>().unwrap());
 }
