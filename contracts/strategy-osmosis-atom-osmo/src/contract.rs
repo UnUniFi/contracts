36a881fef28a0458fb109073fb95a95262dc4abe
jununifi <jun.ununifi@outlook.com>
2023-07-21 18:15:51 +0800

    move icq related functions on icq.rs file

--- contracts/strategy-osmosis-atom-osmo/src/contract.rs
+++ contracts/strategy-osmosis-atom-osmo/src/contract.rs
@@ -1,19 +1,17 @@
-use crate::binding::{
-    SudoMsg, UnunifiMsg, BALANCES_PREFIX, BANK_STORE_KEY, GAMM_STORE_KEY, POOLS_PREFIX,
-};
-use crate::helpers::{decode_and_convert, length_prefix};
+use crate::binding::{SudoMsg, UnunifiMsg};
 use crate::ica::{
-    execute_ibc_transfer_to_controller, execute_ica_add_and_bond_liquidity,
+    determine_ica_amounts, execute_ibc_transfer_to_controller, execute_ica_add_and_bond_liquidity,
     execute_ica_begin_unbonding_lp_tokens, execute_ica_remove_liquidity,
     execute_ica_swap_balance_to_two_tokens, execute_ica_swap_two_tokens_to_deposit_token,
 };
+use crate::icq::{submit_icq_for_host, sudo_kv_query_result, sudo_transfer_callback};
 use crate::msg::{
     ChannelResponse, ExecuteMsg, FeeInfo, InstantiateMsg, ListChannelsResponse, MigrateMsg,
     QueryMsg, UpdateConfigMsg,
 };
 use crate::state::{
-    ChannelInfo, Config, DepositInfo, EpochCallSource, IcaAmounts, Phase, Unbonding, CHANNEL_INFO,
-    CONFIG, DEPOSITS, HOST_LP_RATE_MULTIPLIER, STAKE_RATE_MULTIPLIER, UNBONDINGS,
+    ChannelInfo, Config, DepositInfo, EpochCallSource, Phase, Unbonding, CHANNEL_INFO, CONFIG,
+    DEPOSITS, HOST_LP_RATE_MULTIPLIER, STAKE_RATE_MULTIPLIER, UNBONDINGS,
 };
 use crate::state::{ControllerConfig, HostConfig};
 #[cfg(not(feature = "library"))]
@@ -24,13 +22,9 @@ use cosmwasm_std::{
     Response, StdResult, Storage, Uint128,
 };
 use cw_utils::one_coin;
-use osmosis_std::types::osmosis::gamm::v1beta1::Pool as OsmosisBalancerPool;
 use osmosis_std::types::osmosis::lockup::MsgLockTokensResponse;
 use prost::Message;
-use prost_types::Any;
 use proto::cosmos::base::abci::v1beta1::TxMsgData;
-use proto::cosmos::base::v1beta1::Coin as ProtoCoin;
-use std::str::FromStr;
 use strategy::error::{ContractError, NoDeposit};
 
 //Initialize the contract.
@@ -115,103 +109,6 @@ pub fn sudo(deps: DepsMut, env: Env, msg: SudoMsg) -> Result<Response<UnunifiMsg
             success,
         } => sudo_transfer_callback(deps, env, denom, amount, sender, receiver, memo, success),
     }
-}
-
-pub fn sudo_transfer_callback(
-    deps: DepsMut,
-    env: Env,
-    denom: String,
-    amount: String,
-    sender: String,
-    receiver: String,
-    memo: String,
-    success: bool,
-) -> Result<Response<UnunifiMsg>, ContractError> {
-    deps.api
-        .debug(format!("WASMDEBUG: sudo_transfer_callback received",).as_str());
-    execute_epoch(deps, env, EpochCallSource::TransferCallback, success, None)?;
-    let res = Response::new().add_attribute("action", "ibc_transfer_callback".to_string());
-    return Ok(res);
-}
-
-/// sudo_kv_query_result is the contract's callback for KV query results. Note that only the query
-/// id is provided, so you need to read the query result from the state.
-pub fn sudo_kv_query_result(
-    deps: DepsMut,
-    env: Env,
-    connection_id: String,
-    chain_id: String,
-    query_prefix: String,
-    query_key: Binary,
-    data: Binary,
-) -> Result<Response<UnunifiMsg>, ContractError> {
-    deps.api.debug(
-        format!(
-            "WASMDEBUG: sudo_kv_query_result received; query_id: {:?}",
-            query_key,
-        )
-        .as_str(),
-    );
-
-    let mut config: Config = CONFIG.load(deps.storage)?;
-    let converted_addr_bytes = decode_and_convert(&config.ica_account.as_str())?;
-    let atom_balance_key = create_account_denom_balance_key(
-        converted_addr_bytes.clone(),
-        config.host_config.atom_denom.to_string(),
-    )?;
-    let osmo_balance_key = create_account_denom_balance_key(
-        converted_addr_bytes.clone(),
-        config.host_config.osmo_denom.to_string(),
-    )?;
-    let lp_balance_key = create_account_denom_balance_key(
-        converted_addr_bytes.clone(),
-        config.host_config.lp_denom.to_string(),
-    )?;
-
-    if query_prefix == BANK_STORE_KEY {
-        if query_key == atom_balance_key {}
-        let mut amount = Uint128::from(0u128);
-        if data.len() > 0 {
-            // TODO: to update if Osmosis update Cosmos version to v0.47
-            let balance: ProtoCoin = ProtoCoin::decode(data.as_slice())?;
-            amount = Uint128::from_str(balance.amount.as_str())?;
-        }
-        if query_key == atom_balance_key {
-            config.host_config.free_atom_amount = amount;
-        } else if query_key == osmo_balance_key {
-            config.host_config.free_osmo_amount = amount;
-        } else if query_key == lp_balance_key {
-            config.host_config.free_lp_amount = amount;
-        }
-    } else {
-        // GAMM_STORE_KEY
-        let any: Any = Any::decode(data.as_slice())?;
-        let pool: OsmosisBalancerPool = OsmosisBalancerPool::decode(any.value.as_slice())?;
-        let mut atom_amount = Uint128::from(0u128);
-        let mut total_share = Uint128::from(0u128);
-        for pool_asset in pool.pool_assets {
-            if let Some(token) = pool_asset.token {
-                if token.denom == config.host_config.atom_denom.to_string() {
-                    atom_amount = Uint128::from_str(token.amount.as_str())?;
-                    break;
-                }
-            }
-        }
-        if let Some(total_shares) = pool.total_shares {
-            total_share = Uint128::from_str(total_shares.amount.as_str())?;
-        }
-        config.host_config.lp_redemption_rate =
-            atom_amount * Uint128::from(2u128) * HOST_LP_RATE_MULTIPLIER / total_share;
-    }
-
-    config.pending_icq -= 1;
-    CONFIG.save(deps.storage, &config)?;
-
-    if config.pending_icq == 0 {
-        execute_epoch(deps, env, EpochCallSource::IcqCallback, true, None)?;
-    }
-
-    Ok(Response::default())
 }
 
 //Execute the handle messages.
@@ -312,75 +209,6 @@ pub fn execute_update_config(
     Ok(resp)
 }
 
-// Regular epoch operation (once per day)
-// - icq balance of ica account when `Deposit` phase
-// Unbonding epoch operation
-// - begin lp unbonding on host through ica tx per unbonding epoch - per day probably - (if to unbond lp is not enough, wait for icq to update bonded lp correctly)
-// `Deposit` phase operations
-// - This phase starts when `WithdrawToUser` phase ends
-// - ibc transfer to host for newly incoming atoms
-// - ibc transfer to host for stacked atoms during withdraw phases
-// - swap half atom to osmo & half osmo to atom in a single ica tx
-// - initiate and wait for icq to update latest balances
-// - add liquidity & bond in a single ica tx
-// - repeat the flow
-// `DepositEnding` phase operations
-// - This phase starts from `Deposit` phase, when ica free lp balance is positive
-// - ibc transfers are disabled
-// - swap half atom to osmo & half osmo to atom in a single ica tx
-// - wait for icq to update latest balances
-// - add liquidity & bond in a single ica tx
-// - initiate and wait for icq to update latest balances
-// - update to phase to `LqWithdraw`
-// `Withdraw` phase operations
-// - This phase starts when `DepositEnding` phase ends
-// - Mark unbond ending queue items on contract
-// - execute remove liquidity operation
-// - initiate and wait or icq to update latest balances
-// - swap full osmo to atom
-// - initiate and wait or icq to update latest balances
-// - ibc transfer full atom balance from ica to contract
-// - wait for ica callback for ibc transfer finalization
-// - calculate amount to return, contract balance - stacked atom balance for deposit
-// - send amounts to marked unbond ending items proportionally
-// - switch to `Deposit` phase
-pub fn determine_ica_amounts(config: Config) -> IcaAmounts {
-    if config.phase == Phase::Withdraw {
-        let mut amount_to_return = Uint128::from(0u128);
-        if config.controller_config.free_amount > config.controller_config.stacked_amount_to_deposit
-        {
-            amount_to_return = config.controller_config.free_amount
-                - config.controller_config.stacked_amount_to_deposit;
-        }
-        return IcaAmounts {
-            to_swap_atom: Uint128::from(0u128),
-            to_swap_osmo: config.host_config.free_atom_amount,
-            to_remove_lp: config.host_config.free_lp_amount,
-            to_add_lp: Uint128::from(0u128),
-            to_transfer_to_controller: config.host_config.free_atom_amount,
-            to_transfer_to_host: Uint128::from(0u128),
-            to_return_amount: amount_to_return,
-        };
-    } else {
-        let to_add_lp = config.host_config.free_atom_amount
-            * HOST_LP_RATE_MULTIPLIER
-            * Uint128::from(2u128)
-            * Uint128::from(9u128)
-            / Uint128::from(10u128)
-            / config.host_config.lp_redemption_rate;
-
-        return IcaAmounts {
-            to_swap_atom: config.host_config.free_atom_amount / Uint128::from(2u128),
-            to_swap_osmo: config.host_config.free_osmo_amount / Uint128::from(2u128),
-            to_add_lp: to_add_lp,
-            to_remove_lp: Uint128::from(0u128),
-            to_transfer_to_controller: Uint128::from(0u128),
-            to_transfer_to_host: config.controller_config.free_amount,
-            to_return_amount: Uint128::from(0u128),
-        };
-    }
-}
-
 pub fn execute_stake(
     deps: DepsMut,
     env: Env,
@@ -417,92 +245,6 @@ pub fn execute_stake(
         .add_attribute("sender", sender)
         .add_attribute("amount", amount);
     Ok(rsp)
-}
-
-/// Creates balances Cosmos-SDK storage prefix for account with **addr**
-/// https://github.com/cosmos/cosmos-sdk/blob/ad9e5620fb3445c716e9de45cfcdb56e8f1745bf/x/bank/types/key.go#L55
-pub fn create_account_balances_prefix<AddrBytes: AsRef<[u8]>>(
-    addr: AddrBytes,
-) -> Result<Vec<u8>, ContractError> {
-    let mut prefix: Vec<u8> = vec![BALANCES_PREFIX];
-    prefix.extend_from_slice(length_prefix(addr)?.as_slice());
-
-    Ok(prefix)
-}
-
-/// Creates **denom** balance Cosmos-SDK storage key for account with **addr**
-pub fn create_account_denom_balance_key<AddrBytes: AsRef<[u8]>, S: AsRef<str>>(
-    addr: AddrBytes,
-    denom: S,
-) -> Result<Vec<u8>, ContractError> {
-    let mut account_balance_key = create_account_balances_prefix(addr)?;
-    account_balance_key.extend_from_slice(denom.as_ref().as_bytes());
-
-    Ok(account_balance_key)
-}
-
-pub fn create_pool_key(pool_id: u64) -> Result<Vec<u8>, ContractError> {
-    let mut pool_key: Vec<u8> = vec![POOLS_PREFIX];
-    pool_key.extend_from_slice(pool_id.to_be_bytes().as_slice());
-
-    Ok(pool_key)
-}
-
-// Submit the ICQ for the withdrawal account balance
-pub fn submit_icq_for_host(
-    store: &mut dyn Storage,
-    env: Env,
-) -> Result<Response<UnunifiMsg>, ContractError> {
-    let mut config: Config = CONFIG.load(store)?;
-    config.pending_icq = 4u64;
-    CONFIG.save(store, &config)?;
-
-    let converted_addr_bytes = decode_and_convert(&config.ica_account.as_str())?;
-
-    let atom_balance_key = create_account_denom_balance_key(
-        converted_addr_bytes.clone(),
-        config.host_config.atom_denom,
-    )?;
-    let osmo_balance_key = create_account_denom_balance_key(
-        converted_addr_bytes.clone(),
-        config.host_config.osmo_denom,
-    )?;
-    let lp_balance_key = create_account_denom_balance_key(
-        converted_addr_bytes.clone(),
-        config.host_config.lp_denom,
-    )?;
-    let gamm_pool_key = create_pool_key(config.host_config.pool_id)?;
-
-    let msgs = vec![
-        UnunifiMsg::SubmitICQRequest {
-            chain_id: config.host_config.chain_id.to_string(),
-            connection_id: config.ica_connection_id.to_string(),
-            query_prefix: BANK_STORE_KEY.to_string(),
-            query_key: Binary(atom_balance_key),
-        },
-        UnunifiMsg::SubmitICQRequest {
-            chain_id: config.host_config.chain_id.to_string(),
-            connection_id: config.ica_connection_id.to_string(),
-            query_prefix: BANK_STORE_KEY.to_string(),
-            query_key: Binary(osmo_balance_key),
-        },
-        UnunifiMsg::SubmitICQRequest {
-            chain_id: config.host_config.chain_id.to_string(),
-            connection_id: config.ica_connection_id.to_string(),
-            query_prefix: BANK_STORE_KEY.to_string(),
-            query_key: Binary(lp_balance_key),
-        },
-        UnunifiMsg::SubmitICQRequest {
-            chain_id: config.host_config.chain_id.to_string(),
-            connection_id: config.ica_connection_id.to_string(),
-            query_prefix: GAMM_STORE_KEY.to_string(),
-            query_key: Binary(gamm_pool_key),
-        },
-    ];
-
-    // Note: bonded lp and unbonding lp token balance could be managed without icq on contract side
-    let resp = Response::new().add_messages(msgs);
-    return Ok(resp);
 }
 
 pub fn query_balance(
--- contracts/strategy-osmosis-atom-osmo/src/lib.rs
+++ contracts/strategy-osmosis-atom-osmo/src/lib.rs
@@ -3,6 +3,7 @@ pub mod contract;
 pub mod helpers;
 pub mod ibc;
 pub mod ica;
+pub mod icq;
 pub mod msg;
 pub mod state;
 pub mod test_helpers;
