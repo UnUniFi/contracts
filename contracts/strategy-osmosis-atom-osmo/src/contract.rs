9e54b151a3bc22e71fbd0cf4b959e72a6b2995a6
jununifi <jun.ununifi@outlook.com>
2023-07-21 18:05:09 +0800

    move ica related functions on ica.rs file

--- contracts/strategy-osmosis-atom-osmo/src/contract.rs
+++ contracts/strategy-osmosis-atom-osmo/src/contract.rs
@@ -1,7 +1,12 @@
 use crate::binding::{
     SudoMsg, UnunifiMsg, BALANCES_PREFIX, BANK_STORE_KEY, GAMM_STORE_KEY, POOLS_PREFIX,
 };
-use crate::helpers::{decode_and_convert, length_prefix, send_ica_tx};
+use crate::helpers::{decode_and_convert, length_prefix};
+use crate::ica::{
+    execute_ibc_transfer_to_controller, execute_ica_add_and_bond_liquidity,
+    execute_ica_begin_unbonding_lp_tokens, execute_ica_remove_liquidity,
+    execute_ica_swap_balance_to_two_tokens, execute_ica_swap_two_tokens_to_deposit_token,
+};
 use crate::msg::{
     ChannelResponse, ExecuteMsg, FeeInfo, InstantiateMsg, ListChannelsResponse, MigrateMsg,
     QueryMsg, UpdateConfigMsg,
@@ -10,77 +15,23 @@ use crate::state::{
     ChannelInfo, Config, DepositInfo, EpochCallSource, IcaAmounts, Phase, Unbonding, CHANNEL_INFO,
     CONFIG, DEPOSITS, HOST_LP_RATE_MULTIPLIER, STAKE_RATE_MULTIPLIER, UNBONDINGS,
 };
-use crate::state::{ControllerConfig, HostConfig, InterchainAccountPacketData};
+use crate::state::{ControllerConfig, HostConfig};
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
     coin, coins, to_binary, Addr, BalanceResponse, BankMsg, BankQuery, Binary, Coin, CosmosMsg,
-    Decimal, Deps, DepsMut, Env, IbcMsg, IbcTimeout, MessageInfo, Order, QuerierWrapper,
-    QueryRequest, Response, StdError, StdResult, Storage, Uint128,
+    Decimal, Deps, DepsMut, Env, IbcTimeout, MessageInfo, Order, QuerierWrapper, QueryRequest,
+    Response, StdResult, Storage, Uint128,
 };
 use cw_utils::one_coin;
-use osmosis_std::shim::Duration;
-use osmosis_std::types::cosmos::base::v1beta1::Coin as OsmosisCoin;
-use osmosis_std::types::osmosis::gamm::poolmodels::balancer::v1beta1::{
-    MsgCreateBalancerPool, MsgCreateBalancerPoolResponse,
-};
-use osmosis_std::types::osmosis::gamm::v1beta1::{
-    MsgExitPool, MsgExitPoolResponse, MsgJoinPool, MsgJoinPoolResponse, MsgSwapExactAmountIn,
-    MsgSwapExactAmountInResponse, Pool as OsmosisBalancerPool,
-};
-use osmosis_std::types::osmosis::lockup::{
-    MsgBeginUnlocking,
-    MsgBeginUnlockingAll,
-    MsgBeginUnlockingAllResponse,
-    MsgBeginUnlockingResponse,
-    MsgLockTokens,
-    MsgLockTokensResponse,
-    // MsgSetRewardReceiverAddress, MsgSetRewardReceiverAddressResponse,
-};
-use osmosis_std::types::osmosis::poolmanager::v1beta1::SwapAmountInRoute;
-use prost::{EncodeError, Message};
+use osmosis_std::types::osmosis::gamm::v1beta1::Pool as OsmosisBalancerPool;
+use osmosis_std::types::osmosis::lockup::MsgLockTokensResponse;
+use prost::Message;
 use prost_types::Any;
 use proto::cosmos::base::abci::v1beta1::TxMsgData;
 use proto::cosmos::base::v1beta1::Coin as ProtoCoin;
-use proto::ibc::applications::transfer::v1::MsgTransfer;
-use proto::traits::MessageExt;
 use std::str::FromStr;
 use strategy::error::{ContractError, NoDeposit};
-
-fn join_pool_to_any(msg: MsgJoinPool) -> Result<Any, EncodeError> {
-    return msg.to_bytes().map(|bytes| Any {
-        type_url: "/osmosis.gamm.v1beta1.MsgJoinPool".to_owned(),
-        value: bytes,
-    });
-}
-
-fn exit_pool_to_any(msg: MsgExitPool) -> Result<Any, EncodeError> {
-    return msg.to_bytes().map(|bytes| Any {
-        type_url: "/osmosis.gamm.v1beta1.MsgExitPool".to_owned(),
-        value: bytes,
-    });
-}
-
-fn swap_msg_to_any(msg: MsgSwapExactAmountIn) -> Result<Any, EncodeError> {
-    return msg.to_bytes().map(|bytes| Any {
-        type_url: "/osmosis.gamm.v1beta1.MsgSwapExactAmountIn".to_owned(),
-        value: bytes,
-    });
-}
-
-fn lock_tokens_msg_to_any(msg: MsgLockTokens) -> Result<Any, EncodeError> {
-    return msg.to_bytes().map(|bytes| Any {
-        type_url: "/osmosis.lockup.MsgLockTokens".to_owned(),
-        value: bytes,
-    });
-}
-
-fn begin_unlocking_msg_to_any(msg: MsgBeginUnlocking) -> Result<Any, EncodeError> {
-    return msg.to_bytes().map(|bytes| Any {
-        type_url: "/osmosis.lockup.MsgBeginUnlocking".to_owned(),
-        value: bytes,
-    });
-}
 
 //Initialize the contract.
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -986,249 +937,6 @@ pub fn execute_ibc_transfer_to_host(
         .add_message(ibc_msg)
         .add_attribute("action", "ibc_transfer_to_host");
     Ok(res)
-}
-
-pub fn execute_ibc_transfer_to_controller(
-    store: &mut dyn Storage,
-    env: Env,
-) -> Result<Response<UnunifiMsg>, ContractError> {
-    let config: Config = CONFIG.load(store)?;
-    let ica_amounts = determine_ica_amounts(config.to_owned());
-    let to_transfer_to_controller = ica_amounts.to_transfer_to_controller;
-    if to_transfer_to_controller.is_zero() {
-        return Ok(Response::new());
-    }
-    let msg = MsgTransfer {
-        source_port: "transfer".to_string(),
-        source_channel: config.host_config.transfer_channel_id,
-        token: Some(ProtoCoin {
-            denom: config.host_config.atom_denom,
-            amount: to_transfer_to_controller.to_string(),
-        }),
-        sender: config.ica_account,
-        receiver: env.contract.address.to_string(),
-        timeout_height: None,
-        timeout_timestamp: env.block.time.nanos() + config.transfer_timeout * 1000_000_000,
-    };
-    if let Ok(msg_any) = msg.to_any() {
-        return send_ica_tx(
-            store,
-            env,
-            "transfer_to_controller".to_string(),
-            vec![msg_any],
-        );
-    }
-    Err(ContractError::Std(StdError::SerializeErr {
-        source_type: "proto_any_conversion".to_string(),
-        msg: "".to_string(),
-    }))
-}
-
-pub fn execute_ica_add_and_bond_liquidity(
-    store: &mut dyn Storage,
-    env: Env,
-) -> Result<Response<UnunifiMsg>, ContractError> {
-    let mut config: Config = CONFIG.load(store)?;
-    let share_out_amount = determine_ica_amounts(config.to_owned()).to_add_lp;
-    if share_out_amount.is_zero() {
-        return Ok(Response::new());
-    }
-    config.host_config.pending_bond_lp_amount = share_out_amount;
-    CONFIG.save(store, &config)?;
-
-    let mut tokens_in: Vec<OsmosisCoin> = vec![
-        OsmosisCoin {
-            denom: config.host_config.osmo_denom,
-            amount: config.host_config.free_osmo_amount.to_string(),
-        },
-        OsmosisCoin {
-            denom: config.host_config.atom_denom.to_string(),
-            amount: config.host_config.free_atom_amount.to_string(),
-        },
-    ];
-    tokens_in.sort_by_key(|d| d.denom.to_string());
-
-    let msg1 = MsgJoinPool {
-        sender: config.ica_account.to_string(),
-        share_out_amount: share_out_amount.to_string(),
-        pool_id: config.host_config.pool_id,
-        token_in_maxs: tokens_in,
-    };
-
-    let msg2 = MsgLockTokens {
-        owner: config.ica_account.to_string(),
-        coins: vec![OsmosisCoin {
-            denom: config.host_config.lp_denom,
-            amount: share_out_amount.to_string(),
-        }],
-        duration: Some(Duration {
-            seconds: config.unbond_period as i64,
-            nanos: 0,
-        }),
-    };
-    if let Ok(msg_any1) = join_pool_to_any(msg1) {
-        if let Ok(msg_any2) = lock_tokens_msg_to_any(msg2) {
-            return send_ica_tx(
-                store,
-                env,
-                "add_and_bond_lp_tokens".to_string(),
-                vec![msg_any1, msg_any2],
-            );
-        }
-    }
-    Err(ContractError::Std(StdError::SerializeErr {
-        source_type: "proto_any_conversion".to_string(),
-        msg: "".to_string(),
-    }))
-}
-
-pub fn execute_ica_remove_liquidity(
-    store: &mut dyn Storage,
-    env: Env,
-) -> Result<Response<UnunifiMsg>, ContractError> {
-    let mut config: Config = CONFIG.load(store)?;
-    let ica_amounts = determine_ica_amounts(config.to_owned());
-    let to_remove_lp = ica_amounts.to_remove_lp;
-    if to_remove_lp.is_zero() {
-        return Ok(Response::new());
-    }
-
-    config.host_config.pending_lp_removal_amount = to_remove_lp;
-    CONFIG.save(store, &config)?;
-
-    let mut tokens_out: Vec<OsmosisCoin> = vec![
-        OsmosisCoin {
-            denom: config.host_config.osmo_denom,
-            amount: "1".to_string(),
-        },
-        OsmosisCoin {
-            denom: config.host_config.atom_denom.to_string(),
-            amount: "1".to_string(),
-        },
-    ];
-    tokens_out.sort_by_key(|d| d.denom.to_string());
-
-    let msg = MsgExitPool {
-        sender: config.ica_account.to_string(),
-        share_in_amount: to_remove_lp.to_string(),
-        pool_id: config.host_config.pool_id,
-        token_out_mins: tokens_out,
-    };
-    if let Ok(msg_any) = exit_pool_to_any(msg) {
-        return send_ica_tx(store, env, "remove_liquidity".to_string(), vec![msg_any]);
-    }
-    Err(ContractError::Std(StdError::SerializeErr {
-        source_type: "proto_any_conversion".to_string(),
-        msg: "".to_string(),
-    }))
-}
-
-pub fn execute_ica_swap_two_tokens_to_deposit_token(
-    store: &mut dyn Storage,
-    env: Env,
-) -> Result<Response<UnunifiMsg>, ContractError> {
-    let config: Config = CONFIG.load(store)?;
-    let ica_amounts = determine_ica_amounts(config.to_owned());
-    let to_swap_osmo = ica_amounts.to_swap_osmo;
-    if to_swap_osmo.is_zero() {
-        return Ok(Response::new());
-    }
-    let msg = MsgSwapExactAmountIn {
-        sender: config.ica_account.to_string(),
-        token_in: Some(OsmosisCoin {
-            denom: config.host_config.osmo_denom,
-            amount: to_swap_osmo.to_string(),
-        }),
-        token_out_min_amount: "1".to_string(),
-        routes: vec![SwapAmountInRoute {
-            pool_id: config.host_config.pool_id,
-            token_out_denom: config.host_config.atom_denom,
-        }],
-    };
-    if let Ok(msg_any) = swap_msg_to_any(msg) {
-        return send_ica_tx(store, env, "swap_to_atom".to_string(), vec![msg_any]);
-    }
-    Err(ContractError::Std(StdError::SerializeErr {
-        source_type: "proto_any_conversion".to_string(),
-        msg: "".to_string(),
-    }))
-}
-
-pub fn execute_ica_swap_balance_to_two_tokens(
-    store: &mut dyn Storage,
-    env: Env,
-) -> Result<Response<UnunifiMsg>, ContractError> {
-    let config: Config = CONFIG.load(store)?;
-    let ica_amounts = determine_ica_amounts(config.to_owned());
-    let to_swap_atom = ica_amounts.to_swap_atom;
-    let to_swap_osmo = ica_amounts.to_swap_osmo;
-
-    let mut msgs: Vec<Any> = vec![];
-    if !to_swap_osmo.is_zero() {
-        let msg = MsgSwapExactAmountIn {
-            sender: config.ica_account.to_string(),
-            token_in: Some(OsmosisCoin {
-                denom: config.host_config.osmo_denom.to_string(),
-                amount: to_swap_osmo.to_string(),
-            }),
-            token_out_min_amount: "1".to_string(),
-            routes: vec![SwapAmountInRoute {
-                pool_id: config.host_config.pool_id,
-                token_out_denom: config.host_config.atom_denom.to_string(),
-            }],
-        };
-        if let Ok(msg_any) = swap_msg_to_any(msg) {
-            msgs.push(msg_any);
-        }
-    }
-
-    if !to_swap_atom.is_zero() {
-        let msg = MsgSwapExactAmountIn {
-            sender: config.ica_account.to_string(),
-            token_in: Some(OsmosisCoin {
-                denom: config.host_config.atom_denom,
-                amount: to_swap_atom.to_string(),
-            }),
-            token_out_min_amount: "1".to_string(),
-            routes: vec![SwapAmountInRoute {
-                pool_id: config.host_config.pool_id,
-                token_out_denom: config.host_config.osmo_denom,
-            }],
-        };
-        if let Ok(msg_any) = swap_msg_to_any(msg) {
-            msgs.push(msg_any);
-        }
-    }
-    if msgs.len() > 0 {
-        return send_ica_tx(store, env, "swap_to_lp_underlyings".to_string(), msgs);
-    }
-    return Ok(Response::new());
-}
-
-pub fn execute_ica_begin_unbonding_lp_tokens(
-    store: &mut dyn Storage,
-    env: Env,
-    unbonding_lp_amount: Uint128,
-) -> Result<Response<UnunifiMsg>, ContractError> {
-    let config: Config = CONFIG.load(store)?;
-    if unbonding_lp_amount.is_zero() {
-        return Ok(Response::new());
-    }
-    let msg = MsgBeginUnlocking {
-        owner: config.ica_account.to_string(),
-        id: config.host_config.lock_id,
-        coins: vec![OsmosisCoin {
-            denom: config.host_config.lp_denom,
-            amount: unbonding_lp_amount.to_string(),
-        }],
-    };
-    if let Ok(msg_any) = begin_unlocking_msg_to_any(msg) {
-        return send_ica_tx(store, env, "begin_unbonding_lp".to_string(), vec![msg_any]);
-    }
-    Err(ContractError::Std(StdError::SerializeErr {
-        source_type: "proto_any_conversion".to_string(),
-        msg: "".to_string(),
-    }))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
--- contracts/strategy-osmosis-atom-osmo/src/lib.rs
+++ contracts/strategy-osmosis-atom-osmo/src/lib.rs
@@ -2,6 +2,7 @@ pub mod binding;
 pub mod contract;
 pub mod helpers;
 pub mod ibc;
+pub mod ica;
 pub mod msg;
 pub mod state;
 pub mod test_helpers;
