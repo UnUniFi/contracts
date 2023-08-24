4b5a1539a36cfe9c528be2829cc41d02e2eafddc
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-08-24 17:23:15 +0900

    feat: send back

--- contracts/outpost-nftbackedloan-internal/src/execute/send_back.rs
+++ contracts/outpost-nftbackedloan-internal/src/execute/send_back.rs
@@ -1,5 +1,7 @@
 use crate::error::ContractError;
 use crate::msgs::SendBackMsg;
+use cosmwasm_std::to_binary;
+use cosmwasm_std::CosmosMsg;
 use cosmwasm_std::{DepsMut, Env, MessageInfo, Response};
 
 #[cfg(not(feature = "library"))]
@@ -9,9 +11,18 @@ pub fn execute_send_back(
     info: MessageInfo,
     msg: SendBackMsg,
 ) -> Result<Response, ContractError> {
+
     let mut response = Response::new();
 
-    // TODO: send IBC packet to the external contract
+    //  send IBC packet to the external contract
+    response = response.add_message(CosmosMsg::Wasm(cosmwasm_std::WasmMsg::Execute {
+        contract_addr: msg.origin_address,
+        msg: to_binary(&cw721::Cw721ExecuteMsg::TransferNft {
+            recipient: env.contract.address.to_string(),
+            token_id: msg.token_id,
+        })?,
+        funds: vec![],
+    }));
 
     Ok(response)
 }
