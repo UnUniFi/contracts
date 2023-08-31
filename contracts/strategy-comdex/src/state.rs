8d0425597d41292c9a3e8618899dffc593425862
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-08-31 15:30:41 +0800

    feat: remove comdex strategy

--- contracts/strategy-comdex/src/state.rs
+++
@@ -1,8 +0,0 @@
-use crate::types::{ChannelInfo, Config, DepositInfo};
-use cw_storage_plus::{Item, Map};
-
-pub const CONFIG: Item<Config> = Item::new("config");
-
-pub const DEPOSITS: Map<String, DepositInfo> = Map::new("deposits");
-
-pub const CHANNEL_INFO: Map<&str, ChannelInfo> = Map::new("channel_info");
