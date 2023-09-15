cb872be4400886768896e342cb049d3b9a9f4315
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-09-15 20:30:12 +0900

    feat: eth testnet

--- contracts/outpost-yieldaggregator-external-axelar/hardhat.config.ts
+++ contracts/outpost-yieldaggregator-external-axelar/hardhat.config.ts
@@ -1,8 +1,30 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+// Go to https://infura.io, sign up, create a new API key
+// in its dashboard, and replace "KEY" with it
+const INFURA_KEY = "";
+
+// Replace this private key with your Sepolia account private key
+// To export your private key from Coinbase Wallet, go to
+// Settings > Developer Settings > Show private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const PRIV_KEY = "";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    goerli: {
+      url: "https://goerli.infura.io/v3/${INFURA_KEY}",
+      accounts: [PRIV_KEY],
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_KEY}`,
+      accounts: [PRIV_KEY],
+    },
+  },
 };
 
 export default config;
